2a7600a091bf42a2b5e6a9cce75dd4bef507da8b
Nestor Recinos <00083120@uca.edu.sv>
2022-05-16 22:15:02 -0600

    :card_file_box:  Add fields to User entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,22 @@
-import { Entity } from "typeorm";
+import { Column, Entity, ObjectID } from "typeorm";
 
 @Entity()
-export class User { }
+export class User { 
+  @Column()
+  id: ObjectID;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  birthDate: Date;
+}
